9ad9ec2fdba73bd1b4fa7fd012389d2fc95188a5
yamanobori_old <yamanobori_old@yahoo.co.jp>
2016-11-23 11:49:48 +0900

    初版

--- ConvClipboardPath/Program.cs
+++ ConvClipboardPath/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConvClipboardPath
 {
     class Program
     {
+        private static readonly string usage =
+            @"Usage cmd [OPTION]" + "\n" +
+            @"-bs    '\'BackSlash to '/'Slash" + "\n" +
+            @"-hz    '\'hankaku to '￥'zenkaku" + "\n";
+        [STAThread]
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.Write(usage);
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-bs":
+                        Clipboard.SetText(bsTos(Clipboard.GetText()));
+                        break;
+                    case "-hz":
+                        Clipboard.SetText(hankakuToZenkaku(Clipboard.GetText()));
+                        break;
+                }
+            }
+        }
+        static private string surround(string input)
+        {
+            if (input.Contains(" "))
+            {
+                return "\"" + input + "\"";
+            }
+            return input;
+        }
+        static private string bsTos(string input)
+        {
+            return surround(input).Replace("\\", "/");
+        }
+        static private string hankakuToZenkaku(string input)
+        {
+            return surround(input).Replace("\\", "￥");
         }
     }
 }
