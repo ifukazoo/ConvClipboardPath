9860f88113367fa25a97c7c502325a3d4d4d4c03
yamanobori_old <yamanobori_old@yahoo.co.jp>
2017-02-12 20:21:55 +0900

    全角にする際は、スラッシュも全角にする

--- ConvClipboardPath/Program.cs
+++ ConvClipboardPath/Program.cs
@@ -9,41 +9,41 @@ namespace ConvClipboardPath
             @"Usage cmd [OPTION]" + "\n" +
             @"-bs    '\'BackSlash to '/'Slash" + "\n" +
             @"-hz    '\'hankaku to '￥'zenkaku" + "\n";
+
         [STAThread]
         static void Main(string[] args)
         {
             if (args.Length == 0)
             {
                 Console.Write(usage);
+                Environment.Exit(1);
             }
-            else
+
+            switch (args[0])
             {
-                switch (args[0])
-                {
-                    case "-bs":
-                        Clipboard.SetText(bsTos(Clipboard.GetText()));
-                        break;
-                    case "-hz":
-                        Clipboard.SetText(hankakuToZenkaku(Clipboard.GetText()));
-                        break;
-                }
+                case "-bs":
+                    Clipboard.SetText(bsTos(Clipboard.GetText()));
+                    break;
+                case "-hz":
+                    Clipboard.SetText(hankakuToZenkaku(Clipboard.GetText()));
+                    break;
             }
         }
         static private string surround(string input)
         {
             if (input.Contains(" "))
             {
-                return "\"" + input + "\"";
+                return '"' + input + '"';
             }
             return input;
         }
         static private string bsTos(string input)
         {
-            return surround(input).Replace("\\", "/");
+            return surround(input).Replace(@"\", "/");
         }
         static private string hankakuToZenkaku(string input)
         {
-            return surround(input).Replace("\\", "￥");
+            return surround(input).Replace(@"\", "￥").Replace("/", "￥");
         }
     }
 }
